9432a846422f8c3fa703c68166a249736607a463
Gabriel Ferreira Guimarães <gabrielffguimaraes@gmail.com>
2022-08-11 22:15:07 -0300

    updates

---
+++ src/test/java/org/library/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.library.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
