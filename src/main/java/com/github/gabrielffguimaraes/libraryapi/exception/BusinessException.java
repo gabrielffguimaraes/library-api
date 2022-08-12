3f83192d9b49926ca5c94f5c11dc6410a6a2553e
Gabriel Ferreira Guimarães <62315845+gabrielffguimaraes@users.noreply.github.com>
2022-08-11 22:14:25 -0300

    updates

--- src/main/java/com/github/gabrielffguimaraes/libraryapi/exception/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.github.gabrielffguimaraes.libraryapi.exception;
-
-import com.github.gabrielffguimaraes.libraryapi.model.entity.Book;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String s) {
-        super(s);
-    }
-}
--- src/main/java/com/github/gabrielffguimaraes/libraryapi/service/BookService.java
+++
@@ -1,8 +0,0 @@
-package com.github.gabrielffguimaraes.libraryapi.service;
-
-
-import com.github.gabrielffguimaraes.libraryapi.model.entity.Book;
-
-public interface BookService {
-    Book save(Book book);
-}
